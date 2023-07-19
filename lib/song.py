b2015343dc5cc961dee8107713dac3a93b45aee6
Giselle <gisellec60@gmail.com>
2023-07-19 17:19:34 -0400

    not able to pass last two tests

--- lib/song.py
+++ lib/song.py
@@ -19,7 +19,6 @@ class Song:
     @classmethod
     def add_song_to_count(cls):
         cls.count = cls.count + 1
-        print(cls.count)
         return cls.count
     
     @classmethod
@@ -44,6 +43,7 @@ class Song:
                 if isinstance(artist, str):
                    if self.artist == artist: 
                         cls.artists[x+1] = cls.artists[x+1] + 1
+        # print("this is artists", cls.artists)
         return cls.artists
     
     @classmethod  
@@ -51,14 +51,13 @@ class Song:
         genre_count = {cls.genres[i]: cls.genres[i+1] for i in range(0,len(cls.genres),2)}    
         cls.genre_count=genre_count
         # print("this is the dict", cls.genre_count) 
-        return cls.genre_count
+        return Song.genre_count
     
     @classmethod  
     def add_to_artist_count(cls):
-        artist_count = {cls.artists[i]: cls.artists[i+1] for i in range(0,len(cls.genres),2)}    
+        artist_count = {cls.artists[i]: cls.artists[i+1] for i in range(0,len(cls.artists),2)}    
         cls.artist_count=artist_count
-        # print("this is the dict", cls.genre_count) 
-            
+        # print("this is the dict", cls.artist_count) 
         return cls.artist_count            
 
 # Song("99 Problems", "Jay Z", "Rap")
@@ -66,7 +65,9 @@ class Song:
 # Song("Smells Like Teen Spirit", "Nirvana", "Rock")  
 # Song("Sara Smile", "Hall and Oates", "Pop")
 # Song("Out of Touch", "Hall and Oates", "Pop")
-# Song.add_to_genre_count()
-# print("the list", Song.genres)
-# print("Song.genre_count['Rap'] =", Song.genre_count['Rap'])  
-           
+# # Song.add_to_genre_count()
+# # print("the list", Song.genres)
+# # print("Song.genre_count['Rap'] =", Song.genre_count['Rap'])  
+# # print("the list", Song.artists)
+# Song.add_to_artist_count()
+# print("Song.artist_count['Jay Z'] =", Song.artist_count['Jay Z'])             
