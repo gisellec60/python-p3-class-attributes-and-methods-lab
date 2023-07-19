49ea8ed51d1559bf61f381e1db78b3f2f2f19c34
Giselle <gisellec60@gmail.com>
2023-07-19 19:11:38 -0400

    test finally passed

--- lib/song.py
+++ lib/song.py
@@ -24,50 +24,27 @@ class Song:
     @classmethod
     def add_to_genres(cls, self):
         if self.genre not in cls.genres:
-            cls.genres.extend((self.genre,1))
+            cls.genres.append(self.genre)
+            cls.genre_count[self.genre]=1
         else:
-            for x, genre in enumerate(cls.genres):
-                if isinstance(genre, str):
-                   if self.genre == genre: 
-                        cls.genres[x+1] = cls.genres[x+1] + 1
-                        
-        # print("this is genres", cls.genres)
+            cls.genre_count[self.genre] += 1
         return cls.genres
-    
+         
     @classmethod
     def add_to_artists(cls, self):
         if self.artist not in cls.artists:
-            cls.artists.extend((self.artist,1))
+            cls.artists.append(self.artist)
+            cls.artist_count[self.artist]=1
         else:
-            for x, artist in enumerate(cls.artists):
-                if isinstance(artist, str):
-                   if self.artist == artist: 
-                        cls.artists[x+1] = cls.artists[x+1] + 1
-        # print("this is artists", cls.artists)
+           cls.artist_count[self.artist] += 1
         return cls.artists
     
     @classmethod  
     def add_to_genre_count(cls):
-        genre_count = {cls.genres[i]: cls.genres[i+1] for i in range(0,len(cls.genres),2)}    
-        cls.genre_count=genre_count
-        # print("this is the dict", cls.genre_count) 
-        return Song.genre_count
+        return cls.genre_count
     
     @classmethod  
     def add_to_artist_count(cls):
-        artist_count = {cls.artists[i]: cls.artists[i+1] for i in range(0,len(cls.artists),2)}    
-        cls.artist_count=artist_count
         # print("this is the dict", cls.artist_count) 
         return cls.artist_count            
 
-# Song("99 Problems", "Jay Z", "Rap")
-# Song("Halo", "Beyonce", "Pop")
-# Song("Smells Like Teen Spirit", "Nirvana", "Rock")  
-# Song("Sara Smile", "Hall and Oates", "Pop")
-# Song("Out of Touch", "Hall and Oates", "Pop")
-# # Song.add_to_genre_count()
-# # print("the list", Song.genres)
-# # print("Song.genre_count['Rap'] =", Song.genre_count['Rap'])  
-# # print("the list", Song.artists)
-# Song.add_to_artist_count()
-# print("Song.artist_count['Jay Z'] =", Song.artist_count['Jay Z'])             
